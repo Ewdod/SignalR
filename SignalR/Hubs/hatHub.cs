4ee4f62b728e7ac9f0c6ceeea8f5ec3042598c6b
Ewdod <haybekbatuhan@gmail.com>
2023-10-17 19:51:57 +0300

    Add project files.

---
+++ SignalR/Hubs/hatHub.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalR.Hubs
+{
+    public class hatHub : Hub
+    {
+        public async Task MesajGonder(string kullanici, string mesaj)
+        {
+            await Clients.All.SendAsync("mesajal", kullanici, mesaj);
+        }
+
+        public async Task KonumGonder(string renk, int x, int y)
+        {
+
+            await Clients.All.SendAsync("KonumAlindi",Context.ConnectionId, renk, x, y);
+        }
+    }
+}
